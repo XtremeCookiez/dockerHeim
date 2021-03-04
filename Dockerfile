42675feaaa6a6014778aa2c0ace8ea0f6fd23819
Cameron Winter <cameron@nonya.com>
2021-03-03 23:32:58 -0600

    Updated dockerfile for actually saving the world

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ FROM ubuntu:20.04
 EXPOSE 2456
 
 VOLUME /root/app
+VOLUME /world_data
 
 COPY startup_script.sh /root/startup_script.sh
 RUN chmod +x /root/startup_script.sh
 
 ENV SERVER_NAME=MyCoolServer
-ENV WORLD=MyWorld
+ENV WORLD_NAME=MyWorld
 ENV PASSWORD=Password
 
 WORKDIR /root/app
--- startup_script.sh
+++ startup_script.sh
@@ -9,7 +9,7 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name "My server" -port 2456 -world "Dedicated" -password "secret"
+./valheim_server.x86_64 -name $SERVER_NAME -port 2456 -world $WORLD_NAME -password $PASSWORD -savedir /world_data
 
 export LD_LIBRARY_PATH=$templdpath
 
